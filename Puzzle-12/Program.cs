080b0d2bcbb6d7361d39b83bc3dadbfd46e81b6c
Diego Faria <diego.rdfaria@gmail.com>
2023-11-16 00:08:27 -0500

    Injecting MyBackgroundTask in Hosted Service

--- Puzzle-12/Program.cs
+++ Puzzle-12/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddSingleton<MyBackgroundTask>();
+builder.Services.AddHostedService<MyBackgroundTask>();
 
 var app = builder.Build();
 
